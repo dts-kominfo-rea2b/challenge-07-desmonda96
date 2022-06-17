2f8352aaaa39a74d099b3fb12f627597a01df089
Desmonda <diera.desmonda@gmail.com>
2022-06-17 14:47:59 +0700

    new function

--- promise.js
+++ promise.js
@@ -1,7 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    if (emosi == "") {
+      reject("Invalid Data!");
+    } else if (emosi == "marah") {
+      (async () => {
+        const sumIIX = hitung(await promiseTheaterIXX(), emosi);
+        const sumVGC = hitung(await promiseTheaterVGC(), emosi);
+        resolve(sumIIX + sumVGC);
+      })();
+    } else if (emosi == "tidak marah") {
+      (async () => {
+        const sumIIX = hitung(await promiseTheaterIXX(), emosi);
+        const sumVGC = hitung(await promiseTheaterVGC(), emosi);
+        resolve(sumIIX + sumVGC);
+      })();
+    }
+  });
+};
+
 
 module.exports = {
   promiseOutput,
